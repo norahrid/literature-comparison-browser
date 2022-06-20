ec4e1c8857c930d3c1cfc20f88e656a6dfada278
norahrid <norah.ridley@usask.ca>
2022-06-20 12:01:01 -0600

    Add slider to the chunk level

--- src/components/ChunkView.jsx
+++ src/components/ChunkView.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { scaleLinear, interpolateReds, interpolateMagma, interpolateRdBu } from "d3";
+import interact from "interactjs";
 import { changeChunkSelection } from "../reducers/globalSlice";
 import { componentHeight, componentWidth, margin } from "../constants";
 import { 
@@ -16,6 +17,57 @@ const ChunkView = (props) => {
   const chunkSelection = useSelector(state => state["global"]["chunkSelection"]["PRIDE_AND_PREJUDICE"]);
   const { low, high } = findBoundariesOfCharacteristic(bookData, "length");
   const data = bookData[chunkSelection];
+
+  const attachDraggableSlider = () => {
+
+    interact('#chunk-slider')
+      .draggable({
+        inertia: true,
+        listeners: {
+          move(event) {
+            var target = event.target;
+            var x = (parseFloat(target.getAttribute('data-x')) || 0);
+            x += event.dx;
+            if (x >= 0 && x <= componentWidth) {
+                target.style.webkitTransform = target.style.transform =
+                    'translate(' + x + 'px,' + '0px)'
+                target.setAttribute('data-x', x);
+            }
+          },
+          end(event) {console.log(event)}
+        },
+      })
+      .resizable({
+        // resize from all edges and corners
+        edges: { left: true, right: true, bottom: false, top: false },
+        listeners: {
+            move(event) {
+              var target = event.target;
+              var x = (parseFloat(target.getAttribute('data-x')) || 0);
+              // update the element's style
+              target.style.width = event.rect.width + 'px';
+              // translate when resizing from left edges
+              x += event.deltaRect.left;
+              target.style.webkitTransform = target.style.transform =
+                  'translate(' + x + 'px,' + '0px)'
+              target.setAttribute('data-x', x);
+            },
+            end(event) { console.log(event) }
+        },
+        modifiers: [
+            // keep the edges inside the parent
+            interact.modifiers.restrictEdges({
+                outer: 'parent'
+            }),
+            // minimum size
+            interact.modifiers.restrictSize({
+                min: { width: 30 }
+            })
+        ],
+        inertia: true
+      })
+
+  }
 
   
 
@@ -34,6 +86,8 @@ const ChunkView = (props) => {
         ctx.fillStyle = colour;
         ctx.fillRect(i*unit, 0, unit, componentHeight);
     }
+
+    attachDraggableSlider();
   }
 
   const selectChunk = (event) => {
@@ -52,14 +106,25 @@ const ChunkView = (props) => {
   }, [draw]);
   
   return (
-    <canvas 
-      className="global-view" 
-      width={componentWidth} 
-      height={componentHeight}
-      onClick={selectChunk} 
-      ref={canvasRef} 
-      {...props} 
-    />
+    <div className="chunk-outer-wrapper">
+      <div className="chunk-inner-wrapper" style={{'width': componentWidth}}>
+        <div className="chunk-window-wrapper" style={{'width': componentWidth}}>
+          <div id="chunk-slider"
+            style={{ height: componentHeight + 'px' }}>
+          </div>
+        </div>
+      </div>
+      <canvas 
+        className="chunk-view" 
+        width={componentWidth} 
+        height={componentHeight}
+        onClick={selectChunk} 
+        ref={canvasRef} 
+        {...props} 
+      />
+    </div>
+
+
   )
 };
 export default ChunkView;
