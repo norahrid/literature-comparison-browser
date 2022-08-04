7735f02b4f901e91660b94444d65ba7e5885462b
norahrid <norah.ridley@usask.ca>
2022-08-04 10:59:20 -0600

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,11 @@ npm run build
 sudo systemctl stop nginx
 
 # clear old assets
-rm -rf /var/www/repeat-map/
+rm -rf /var/www/visual-comparison-browser/
 # rm -rf /var/www/repeat-map-secret/
 
 # copy new assets
-cp -a build/. /var/www/repeat-map/
+cp -a build/. /var/www/visual-comparison-browser/
 # cp -a build/. /var/www/repeat-map-secret/
 
 # restart nginx server
